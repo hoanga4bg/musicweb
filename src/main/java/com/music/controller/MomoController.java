d848cea8698715599cc20b3416df7e681071d0bf
hoanga4bg <hoanga4bg@gmail.com>
2021-10-01 17:26:07 +0700

    1

--- src/main/java/com/music/controller/MomoController.java
+++ src/main/java/com/music/controller/MomoController.java
@@ -16,7 +16,7 @@ import java.util.Date;
 import java.util.HashMap;
 
 @RestController
-@CrossOrigin
+@CrossOrigin("*")
 public class MomoController {
 
     @Autowired
@@ -26,25 +26,23 @@ public class MomoController {
     private IAccountDAO accountDAO;
     @PostMapping("momo/notifyUrl")
     public HashMap notifyUrl(
-            @RequestParam(defaultValue = "") String partnerCode,
-            @RequestParam(defaultValue = "") String accessKey,
-            @RequestParam(defaultValue = "") String requestId,
-            @RequestParam(defaultValue = "") String amount,
-            @RequestParam(defaultValue = "") String orderId,
-            @RequestParam(defaultValue = "") String orderInfo,
-            @RequestParam(defaultValue = "") String orderType,
-            @RequestParam(defaultValue = "") String transId,
-            @RequestParam(defaultValue = "") String errorCode,
-            @RequestParam(defaultValue = "") String message,
-            @RequestParam(defaultValue = "") String localMessage,
-            @RequestParam(defaultValue = "") String payType,
-            @RequestParam(defaultValue = "") String signature,
-            @RequestParam(defaultValue = "") String extraData
+            @RequestParam("partnerCode") String partnerCode,
+            @RequestParam("accessKey") String accessKey,
+            @RequestParam("requestId") String requestId,
+            @RequestParam("amount") String amount,
+            @RequestParam("orderId") String orderId,
+            @RequestParam("transId") String transId,
+            @RequestParam("errorCode") String errorCode,
+            @RequestParam("message") String message,
+            @RequestParam("localMessage") String localMessage,
+            @RequestParam("payType") String payType,
+            @RequestParam("signature") String signature,
+            @RequestParam("extraData") String extraData
     ) {
 
         try {
-            QueryStatusTransactionResponse transactionResponse = moMoService.transactionResponse(orderId, requestId);
-            if (transactionResponse.getErrorCode() == 0) {
+            
+            if (Integer.parseInt(errorCode)==0) {
                 Account a=accountDAO.getLogingAccount();
                 a.setVip(true);
                 accountDAO.save(a);
