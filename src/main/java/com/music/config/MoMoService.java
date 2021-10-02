bc4f1b93ef3513d003e34a3dc69012ac3166212e
hoanga4bg <hoanga4bg@gmail.com>
2021-10-02 11:40:05 +0700

    Update MoMoService.java

--- src/main/java/com/music/config/MoMoService.java
+++ src/main/java/com/music/config/MoMoService.java
@@ -29,7 +29,7 @@ public class MoMoService {
     public String getMoMoPayUrl(String account_id) {
        
             try {
-                String returnUrl ="https://hoangmusic.herokuapp.com/default" ;
+                String returnUrl ="https://hoangmusic.herokuapp.com/" ;
                 return "redirect:" + captureMoMoResponse(account_id,returnUrl).getPayUrl();
             } catch (Exception e) {
                 e.printStackTrace();
